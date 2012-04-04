b62ae6f17e669550a3a168d54f17c4bd3afdce5b
Jack Rogers <jack.rogers@ncf.edu>
2012-04-04 15:17:10 -0300

    outline classes to be made

--- DD.rb
+++ DD.rb
@@ -3,3 +3,22 @@ def lol
 	end
 
 lol()
+
+
+
+
+#classes to be written
+	#Window
+		#gui object
+	#canvas
+		#thing we put in the window
+	#GameMap
+		#thing we put onto the canvas
+	#Dwarf
+	#Item
+		#building material
+		#Weapon
+		#Armor
+	#building
+		#store
+		#tower
